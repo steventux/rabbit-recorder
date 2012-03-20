70ca20a5a56b09c09b01d6cc9bcf874ef9429740
steve <steve@steve-Q430-Q530.(none)>
2012-03-20 21:46:53 +0000

    Bunny machine comments and ouput fixes

--- bunny_machine.rb
+++ bunny_machine.rb
@@ -5,24 +5,31 @@ puts "Starting the bunny machine"
 # Cache move_events
 move_events = []
 
+# Pull a rabbit out of the hat
 b = Bunny.new
 b.start
-# define the queue
+
+# define the movements queue which will consume move events
 queue = b.queue('movements')
-# create/get exchanges
+
+# create/get exchanges for move events and playbacks
 exchange = b.exchange('move-events', :type => :fanout)
 playbacks = b.exchange('playbacks', :type => :fanout)
-# bind queues to exchanges
+
+# bind movements queue to the correct exchange
 queue.bind(exchange)
+
 # listen for messages
+puts "Listening for movements..."
 queue.subscribe do |msg|
-  puts "move-event : #{msg[:payload]}"
+  puts "bunny_machine received : #{msg[:payload]}"
+  # TODO: Better than this
   if msg[:payload] == 'replay'
     puts "replaying #{move_events.size} events"
     move_events.each { |e| playbacks.publish(e) }
     move_events = []
   else 
     move_events << msg[:payload]
-    puts "#{move_events.size} move events in cache"
+    puts "Cache size : #{move_events.size}"
   end
 end
