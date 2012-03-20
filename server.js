76d005c9639a85ff3ba7016ef4334958e8b4a2e9
steve <steve@steve-Q430-Q530.(none)>
2012-03-20 21:59:15 +0000

    node server comments added

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express');
 var io = require('socket.io');
 var context = require('rabbit.js').createContext();
 
+// Use express at the static file server.
 var app = express.createServer().use(express.static(__dirname + '/public')),
     io = io.listen(app);
-
+    
+// Start the appserver.     
 app.listen(8080);
 
 io.sockets.on('connection', function(socket) {
-
+  
+  // RabbitJS publisher and subscriber.
   var pub = context.socket('PUB');
   var sub = context.socket('SUB');
 
@@ -24,18 +27,20 @@ io.sockets.on('connection', function(socket) {
     socket.broadcast.emit('move-event', data);
     pub.write(data);
   });
-
+  
+  // Clean up
   socket.on('disconnect', function() {
     pub.destroy();
     sub.destroy();
   });
-
-  // Data sent to the amqp subscriber from RabbitMQ 
+  
+  // Data sent to the amqp subscriber from RabbitMQ. 
   sub.on('data', function(msg) {
-    console.log("Playing back : " + msg)
+    console.info("Playing back : " + msg)
     socket.broadcast.emit('move-event', msg);
   })
 
+  // Connect publisher and subscriber to the relevant amqp exchanges.
   sub.connect('playbacks');
   pub.connect('move-events');
   
