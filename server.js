fc8a250be5ed8ee72899675d951e136a145cfb26
steve <steve@steve-Q430-Q530.(none)>
2012-03-20 21:23:00 +0000

    Now using node as the webserver

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var io = require('socket.io');
 var context = require('rabbit.js').createContext();
 
-var app = express.createServer()
-  , io = io.listen(app);
-  
+var app = express.createServer().use(express.static(__dirname + '/public')),
+    io = io.listen(app);
+
 app.listen(8080);
 
 io.sockets.on('connection', function(socket) {
@@ -14,11 +14,12 @@ io.sockets.on('connection', function(socket) {
 
   sub.setEncoding('utf8');
   
-  // A command from the UI
+  // A command from the UI (e.g. replay)
   socket.on('command', function(data) {
     pub.write(data);
   })
-  // A move event from the UI
+  
+  // A move event from the UI, broadcast this to other clients.
   socket.on('move-event', function (data) {
     socket.broadcast.emit('move-event', data);
     pub.write(data);
@@ -29,9 +30,9 @@ io.sockets.on('connection', function(socket) {
     sub.destroy();
   });
 
-  // Data sent to the amqp subscriber 
+  // Data sent to the amqp subscriber from RabbitMQ 
   sub.on('data', function(msg) {
-    console.log(msg)
+    console.log("Playing back : " + msg)
     socket.broadcast.emit('move-event', msg);
   })
 
