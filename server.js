8ca6c9de6c0492a5a0867ba0dafb94188a6df8ff
Steve <steve.laing@gmail.com>
2012-03-21 12:39:07 +0000

    WIP for the node only idea, seems what comes back from the rabbit needs parsing or sommat

--- server.js
+++ server.js
@@ -11,38 +11,62 @@ app.listen(8080);
 
 io.sockets.on('connection', function(socket) {
   
-  // RabbitJS publisher and subscriber.
-  var pub = context.socket('PUB');
-  var sub = context.socket('SUB');
+  var moveEvents = [];
+  
+  // RabbitJS publishers and subscribers.
+  var pubMe = context.socket('PUB');
+  var subMe = context.socket('SUB');
+  
+  var pubPb = context.socket('PUB');
+  var subPb = context.socket('SUB');
 
-  sub.setEncoding('utf8');
+  subMe.setEncoding('utf8');
   
   // A command from the UI (e.g. replay)
   socket.on('command', function(data) {
-    pub.write(data);
+    pubMe.write(data);
   })
   
   // A move event from the UI, broadcast this to other clients.
   socket.on('move-event', function (data) {
     socket.broadcast.emit('move-event', data);
-    pub.write(data);
+    pubMe.write(data);
   });
   
   // Clean up
   socket.on('disconnect', function() {
-    pub.destroy();
-    sub.destroy();
+    pubMe.destroy();
+    subPb.destroy();
   });
   
+  // Data via the move-events exchange to be cached or persisted.
+  subMe.on('data', function(data) {
+    console.log("Move events subscriber received : " + data)
+    switch(data) {
+      case "replay" :
+        var len = moveEvents.length;
+        if (len) {
+          console.log("Playing back " + len + " events!!!") 
+          for (var i = 0; i < len; i++) pubPb.write(data);
+        }
+        moveEvents = []; // Clear the cache
+        break;
+      default :
+        moveEvents.push(data);
+    }  
+  })
+  
   // Data sent to the amqp subscriber from RabbitMQ. 
-  sub.on('data', function(msg) {
-    console.info("Playing back : " + msg)
-    socket.broadcast.emit('move-event', msg);
+  subPb.on('data', function(data) {
+    console.info("Playing back : " + data)
+    socket.broadcast.emit('move-event', data);
   })
 
   // Connect publisher and subscriber to the relevant amqp exchanges.
-  sub.connect('playbacks');
-  pub.connect('move-events');
+  subPb.connect('playbacks');
+  pubPb.connect('playbacks');
+  subMe.connect('move-events');
+  pubMe.connect('move-events');
   
 });
 
